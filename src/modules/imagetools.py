c8198533f791390b860a31eb999ea140d2516761
0xb1b1 <voxelflee@protonmail.com>
2023-02-19 10:22:40 +0300

    Implement Image Upload Tools and pin versions in PiP deps

--- src/modules/imagetools.py
+++ src/modules/imagetools.py
@@ -1,9 +1,14 @@
-
+import requests as r
+import json
 
 class ImageTools:
     def __init__(self, url):
         self.url = url if url[-1] == "/" else url + "/"
 
-    def upload(image: bytes) -> str:
+    def upload(self, image: bytes) -> str:
         """Uploads image to self.url and returns a link"""
-        pass #! TODO: implement
+        endpoint = self.url + "api/upload.php"
+        # POST file to endpoint
+        response = r.post(endpoint, files={"file": image})
+        return json.loads(response.text)["url"]
+
