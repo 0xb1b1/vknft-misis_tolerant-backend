28614f32fdf4d25baa39c5855b21783bab3061c8
0xb1b1 <voxelflee@protonmail.com>
2023-02-19 05:38:20 +0300

    Create URL postprocessing in ImageTools

--- src/modules/imagetools.py
+++ src/modules/imagetools.py
@@ -2,7 +2,7 @@
 
 class ImageTools:
     def __init__(self, url):
-        self.url = url
+        self.url = url if url[-1] == "/" else url + "/"
 
     def upload(image: bytes) -> str:
         """Uploads image to self.url and returns a link"""
